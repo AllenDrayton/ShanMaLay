afebd28089add83156ba71e963bcfa1d5ea0b476
unknown <allenjackson710@gmail.com>
2023-12-19 13:13:35 -0800

    Browser Autoplay policy fixed

--- pck/scenes/splashScene.gd
+++ pck/scenes/splashScene.gd
@@ -1,11 +1,7 @@
 extends AnimatedSprite
 
 func _ready():
-	frame = 0
-	play("in")
-	$splash.hide()
-	$AudioStreamPlayer.volume_db = 0
-	$AudioStreamPlayer.play()
+	$Door.play("idle")
 
 
 func _on_splashScene_animation_finished():
@@ -17,3 +13,17 @@ func _on_splashScene_animation_finished():
 func _on_splash_animation_finished():
 	$AudioStreamPlayer.volume_db = -80
 	get_tree().change_scene("res://pck/scenes/login.tscn")
+
+
+func _on_openButton_pressed():
+	$openAnimation.play("out")
+	$AudioStreamPlayer.volume_db = 0
+	$AudioStreamPlayer.play()
+
+
+func _on_openAnimation_animation_finished(anim_name):
+	$Door.hide()
+	frame = 0
+	play("in")
+	$splash.hide()
+	
