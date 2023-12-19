d2c8d4be19f30f446cad6405f76b68674fde8c00
Wai Yan Paing @ Hsi Hsi <107989786+WaiYanPaing209@users.noreply.github.com>
2023-12-19 10:00:51 +0630

    setting bug fixed (I think)

--- pck/prefabs/Setting.gd
+++ pck/prefabs/Setting.gd
@@ -4,24 +4,55 @@ extends Panel
 var filepath = "user://setting.txt"
 
 func _ready():
-	var file = File.new()
-	if file.file_exists(filepath):
-		file.open(filepath, File.READ)
-		var data = file.get_as_text()
-		var obj = JSON.parse(data)
-		file.close()
-		var effectVol = obj.result.effect
-		var musicVol = obj.result.music 
-		$SliderEffect.value = effectVol
-		$SliderMusic.value = musicVol
-		AudioServer.set_bus_volume_db(1,effectVol)
-		AudioServer.set_bus_volume_db(2,musicVol)
+	loadSettings()
+#	var file = File.new()
+#	if file.file_exists(filepath):
+#		file.open(filepath, File.READ)
+#		var data = file.get_as_text()
+#		var obj = JSON.parse(data)
+#		file.close()
+#		var effectVol = obj.result.effect
+#		var musicVol = obj.result.music 
+#		$SliderEffect.value = effectVol
+#		$SliderMusic.value = musicVol
+#		AudioServer.set_bus_volume_db(1,effectVol)
+#		AudioServer.set_bus_volume_db(2,musicVol)
 	
 	var canvas_rid = get_canvas_item()
 	# You may need to adjust these values
 	VisualServer.canvas_item_set_draw_index(canvas_rid,100)
 	VisualServer.canvas_item_set_z_index(canvas_rid,100)
 
+
+func loadSettings():
+	var file = File.new()
+	if file.file_exists(filepath):
+		file.open(filepath, File.READ)
+		var savedData = file.get_as_text()
+		file.close()
+
+		var data = JSON.parse(savedData)
+		if data and "music" in data and "effect" in data:
+			var effectVol = data.effect
+			var musicVol = data.music 
+			$SliderEffect.value = effectVol
+			$SliderMusic.value = musicVol
+			AudioServer.set_bus_volume_db(1, effectVol)
+			AudioServer.set_bus_volume_db(2, musicVol)
+		else:
+			print("Invalid or incomplete data structure in the settings file.")
+			# Set default values if needed
+			$SliderEffect.value = 0
+			$SliderMusic.value = 0
+			AudioServer.set_bus_volume_db(1, 0)
+			AudioServer.set_bus_volume_db(2, 0)
+	else:
+		print("Settings file not found.")
+		# Set default values if the file doesn't exist
+		$SliderEffect.value = 0
+		$SliderMusic.value = 0
+		AudioServer.set_bus_volume_db(1, 0)
+		AudioServer.set_bus_volume_db(2, 0)
 
 func _save(data):
 	var file = File.new()
